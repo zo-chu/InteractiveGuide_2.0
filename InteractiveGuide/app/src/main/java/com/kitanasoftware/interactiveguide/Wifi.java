a14c7972d615cb2162dc4f1c90079e228d07e3e0
Chudo <zovut.chudo@gmail.com>
2016-04-08 17:21:54 +0300

    FIxed and seem to be 2.1 version

---
+++ InteractiveGuide/app/src/main/java/com/kitanasoftware/interactiveguide/Wifi.java
@@ -0,0 +1,15 @@
+package com.kitanasoftware.interactiveguide;
+
+public class Wifi {
+
+	public Wifi (){}
+	
+	//IP address, HW type, Flags, HW address, Mask and Device
+	public String IP_ADDRESS = null;
+	public String HARDWARE_TYPE = null;
+	public String FLAGS = null;
+	public String MAC_ADDRESS = null;
+	public String MASK = null;
+	public String DEVICE = null;
+	
+}
